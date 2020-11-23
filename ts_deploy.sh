a8dbe8876cb15ccd201b51a42122a27db0c0d1f6
James <wilfongjt@gmail.com>
2020-11-23 13:22:21 -0500

    #1.Init

--- ts_deploy.sh
+++ ts_deploy.sh
@@ -1,6 +1,7 @@
 # subtree push to heroku
+stge=%1
 # GH_KEY=$1
 # HK_KEY=$1
-git subtree push --prefix lb-hero-app lb-hero-app-stage master
-
-exit 1
+#git subtree push --prefix lb-hero-app lb-hero-app-stage master
+echo "hi $stge"
+exit 0
